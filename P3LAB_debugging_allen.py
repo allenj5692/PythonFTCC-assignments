f43f7a81143bcdfec1b85a11add9d4ea38b816ec
allenj5692 <36580149+allenj5692@users.noreply.github.com>
2018-02-23 00:25:56 -0500

    Add files via upload

---
+++ P3LAB_debugging_allen.py
@@ -0,0 +1,33 @@
+# CTI-110
+# P3LAB - Score to letter grade
+# Allen, Joshua
+# 2/23/2018
+
+
+
+
+# This program takes a number grade and outputs a letter grade.
+# system uses 10-point grading scale
+    
+score = int(input('Enter grade: '))
+
+if score >= 90 and score <= 100:
+    print('Your grade is: A')
+elif score >= 80 and score <= 89:
+    print('Your grade is: B')
+elif score >= 70 and score <= 79:
+    print('Your grade is: C')
+elif score >= 60 and score <= 69:
+    print('Your grade is: D')
+elif score >= 1 and score <= 59:
+    print('Your grade is: F')
+
+
+
+
+
+
+
+
+
+
